f853d712efbccf9dc6addcbecdca4bb25bcdb45d
Emilly <emilly.wanjiku@student.moringaschool.com>
2023-01-25 03:39:31 +0300

    Complete

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,20 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const singleDirector = directors.map((director) =>(
+    <div key={director.name}>
+      <h2>Name: {director.name}</h2>
+      <ul>
+        {director.movies.map((movie) => (
+        <li key={movie}> {movie}</li>
+      ))}
+      </ul>
+    </div>
+  ))
+  return <div>
+    <h1>Directors Page</h1>
+    {singleDirector}
+    </div>;
 }
 
 export default Directors;
